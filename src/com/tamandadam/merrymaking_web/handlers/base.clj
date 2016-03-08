f6fef9bc06f6f0234e405f84b052062d98bdde5b
Adam Tait <adam@adamtait.com>
2016-03-07 22:48:24 -0800

    add resources for Tam's template
    start by stealing from a page that she liked

--- src/com/tamandadam/merrymaking_web/handlers/base.clj
+++ src/com/tamandadam/merrymaking_web/handlers/base.clj
@@ -46,36 +46,23 @@
   (slurp
    (io/resource path)))
 
-(defn index [component request]
+(defn example [component request]
   (let [html-src (load-hiccup-src index-src-path)]
     (str
      (html-doctype-strs)
      html-src)))
 
+
 ;; ------------------------------------------------
-;; ## index path, using Hiccup
+;; ## Template
 
-#_(defn index [component request]
-  (html5
-   [:head
-    [:title "Merrymaking with Tam & Adam"]]
+(def template-src-path "com/tamandadam/merrymaking_web/template.html")
 
-   [:body
-    [:div
-     [:h1 "Merrymaking with Tam & Adam"]]
-    [:div
-     [:h3 "September 17th, 2016"]]
-    [:div
-     [:h3 "San Francisco, California"]]
-
-    [:div "placeholder"]
+(defn index [component request]
+  (load-hiccup-src template-src-path))
 
 
-    [:div "footer"]
-    ]))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ------------------------------------------------
 ;; ## Component API
 
 (defrecord BaseAPI [])
