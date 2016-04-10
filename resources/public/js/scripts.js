1b3141138fea6521dfe935ee827db348f7e792e3
Adam Tait <adam@adamtait.com>
2016-04-10 08:28:08 -0700

    clean up form scripts
    + bonus: remove some other unused code

--- resources/public/js/scripts.js
+++ resources/public/js/scripts.js
@@ -115,12 +115,6 @@ $(document).ready(function() {
     });
 
 
-    // Sliders
-
-    $('.image-slider').flexslider({
-        directionNav: false
-    });
-
 
     //------------------------------------------------
     // Form Scripts
@@ -149,6 +143,13 @@ $(document).ready(function() {
 
     // Checkbox controls
 
+    $('.checkbox-btn').map(function (i, e) {
+        if ( $(this).hasClass('checked') ) {
+            let inputElem = $(this).find('input');
+            $(inputElem).prop('checked', true);
+        }
+    });
+    
     $('.checkbox-btn').click(function() {
         let inputElem = $(this).find('input');
         $(inputElem).prop('checked', !$(this).hasClass('checked'));
@@ -166,135 +167,104 @@ $(document).ready(function() {
     });
 
 
-    // Pickadate (Date picker) controls
-
-    var from_$input = $('#rsvp-arrival-date').pickadate({
-        selectYears: false,
-        hiddenName: true,
-        min: new Date(2016,8,1),
-        max: new Date(2016,8,17)}),
-    from_picker = from_$input.pickadate('picker')
-
-    var to_$input = $('#rsvp-departure-date').pickadate({
-        selectYears: false,
-        hiddenName: true,
-        min: new Date(2016,8,17),
-        max: new Date(2016,9,31)}),
-    to_picker = to_$input.pickadate('picker')
-
-    
-
     // Instagram Feed
 
-    jQuery.fn.spectragram.accessData = {
-        accessToken: '1406933036.fedaafa.feec3d50f5194ce5b705a1f11a107e0b',
-        clientID: 'fedaafacf224447e8aef74872d3820a1'
-    };
+    //jQuery.fn.spectragram.accessData = {
+    //    accessToken: '1406933036.fedaafa.feec3d50f5194ce5b705a1f11a107e0b',
+    //    clientID: 'fedaafacf224447e8aef74872d3820a1'
+    //};
 
-    $('.instafeed').each(function() {
-        $(this).children('ul').spectragram('getUserFeed', {
-            query: $(this).attr('data-user-name')
-        });
-    });
+    //$('.instafeed').each(function() {
+    //    $(this).children('ul').spectragram('getUserFeed', {
+    //        query: $(this).attr('data-user-name')
+    //    });
+    //});
 
     // Contact form code
 
     $('form.rsvp').submit(function(e) {
 
         // return false so form submits through jQuery rather than reloading page.
-        if (e.preventDefault) e.preventDefault();
-        else e.returnValue = false;
+        //if (e.preventDefault)
+            e.preventDefault();
+        //else
+            e.returnValue = false;
 
         var thisForm = $(this).closest('form.rsvp'),
             error = 0,
             originalError = thisForm.attr('original-error'),
             loadingSpinner, iFrame, userEmail, userFullName, userFirstName, userLastName;
 
-        // Mailchimp/Campaign Monitor Mail List Form Scripts
-        iFrame = $(thisForm).find('iframe.mail-list-form');
-
-        if ((iFrame.length) && (typeof iFrame.attr('srcdoc') !== "undefined") && (iFrame.attr('srcdoc') !== "")) {
-
-            userEmail = $(thisForm).find('.signup-email-field').val();
-            userFullName = $(thisForm).find('.signup-name-field').val();
-            userFirstName = $(thisForm).find('.signup-first-name-field').val();
-            userLastName = $(thisForm).find('.signup-last-name-field').val();
-
-            // validateFields returns 1 on error;
-            if (validateFields(thisForm) !== 1) {
-                console.log('Mail list signup form validation passed.');
-                console.log(userEmail);
-                console.log(userLastName);
-                console.log(userFirstName);
-                console.log(userFullName);
-
-                iFrame.contents().find('#mce-EMAIL, #fieldEmail').val(userEmail);
-                iFrame.contents().find('#mce-LNAME, #fieldLastName').val(userLastName);
-                iFrame.contents().find('#mce-FNAME, #fieldFirstName').val(userFirstName);
-                iFrame.contents().find('#mce-FNAME, #fieldName').val(userFullName);
-                iFrame.contents().find('form').attr('target', '_blank').submit();
-            }
-        } else {
-
-            if (typeof originalError !== typeof undefined && originalError !== false) {
-                thisForm.find('.form-error').text(originalError);
-            }
-
-
-            error = validateFields(thisForm);
-
-
-            if (error === 1) {
-                $(this).closest('form').find('.form-error').fadeIn(200);
-                setTimeout(function() {
-                    $(thisForm).find('.form-error').fadeOut(500);
-                }, 3000);
-            } else {
-                // Hide the error if one was shown
-                $(this).closest('form').find('.form-error').fadeOut(200);
-                // Create a new loading spinner while hiding the submit button.
-                loadingSpinner = jQuery('<div />').addClass('form-loading').insertAfter($(thisForm).find('input[type="submit"]'));
-                $(thisForm).find('input[type="submit"]').hide();
-
-                jQuery.ajax({
-                    type: "POST",
-                    url: "http://mailform.mediumra.re/union/mail.php",
-                    data: thisForm.serialize(),
-                    success: function(response) {
-                        // Swiftmailer always sends back a number representing numner of emails sent.
-                        // If this is numeric (not Swift Mailer error text) AND greater than 0 then show success message.
-                        $(thisForm).find('.form-loading').remove();
-                        $(thisForm).find('input[type="submit"]').show();
-                        if ($.isNumeric(response)) {
-                            if (parseInt(response) > 0) {
-                                thisForm.find('.form-success').fadeIn(1000);
-                                thisForm.find('.form-error').fadeOut(1000);
-                                setTimeout(function() {
-                                    thisForm.find('.form-success').fadeOut(500);
-                                }, 5000);
-                            }
-                        }
-                        // If error text was returned, put the text in the .form-error div and show it.
-                        else {
-                            // Keep the current error text in a data attribute on the form
-                            thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
-                            // Show the error with the returned error text.
-                            thisForm.find('.form-error').text(response).fadeIn(1000);
-                            thisForm.find('.form-success').fadeOut(1000);
-                        }
-                    },
-                    error: function(errorObject, errorText, errorHTTP) {
-                        // Keep the current error text in a data attribute on the form
-                        thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
-                        // Show the error with the returned error text.
-                        thisForm.find('.form-error').text(errorHTTP).fadeIn(1000);
-                        thisForm.find('.form-success').fadeOut(1000);
-                        $(thisForm).find('.form-loading').remove();
-                        $(thisForm).find('input[type="submit"]').show();
-                    }
-                });
-            }
+        if (typeof originalError !== typeof undefined && originalError !== false) {
+            thisForm.find('.form-error').text(originalError);
         }
+
+        //error = validateFields(thisForm);
+
+
+        if (error === 1) {
+            $(this).closest('form').find('.form-error').fadeIn(200);
+            setTimeout(function() {
+                $(thisForm).find('.form-error').fadeOut(500);
+            }, 3000);
+        } else {
+            // Hide the error if one was shown
+            $(this).closest('form').find('.form-error').fadeOut(200);
+            // Create a new loading spinner while hiding the submit button.
+            loadingSpinner = jQuery('<div />').addClass('form-loading').insertAfter($(thisForm).find('input[type="submit"]'));
+            $(thisForm).find('input[type="submit"]').hide();
+
+            var formData = thisForm.serialize();
+            formData.concat(
+                thisForm.find('input[type=checkbox]:checked').map(
+                    function() {
+                        return {"name": this.name, "value": this.value}
+                    }).get()
+            );
+            
+            jQuery.ajax({
+                type: "POST",
+                url: "/rsvp/",
+                data: thisForm.serialize(),
+                success: function(response) {
+                    // Swiftmailer always sends back a number representing numner of emails sent.
+                    // If this is numeric (not Swift Mailer error text) AND greater than 0 then show success message.
+                    $(thisForm).find('.form-loading').remove();
+                    $(thisForm).find('input[type="submit"]').show();
+                    if ($.isNumeric(response)) {
+                        if (parseInt(response) > 0) {
+                            thisForm.find('.form-success').fadeIn(1000);
+                            thisForm.find('.form-error').fadeOut(1000);
+                            setTimeout(function() {
+                                thisForm.find('.form-success').fadeOut(500);
+                            }, 5000);
+                        }
+                    }
+                    // If error text was returned, put the text in the .form-error div and show it.
+                    else {
+                    // Keep the current error text in a data attribute on the form
+                        thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
+                    // Show the error with the returned error text.
+                        thisForm.find('.form-error').text(response).fadeIn(1000);
+                        thisForm.find('.form-success').fadeOut(1000);
+                    }
+
+                    console.log("response: /" + response + "/");
+                },
+                error: function(errorObject, errorText, errorHTTP) {
+                    // Keep the current error text in a data attribute on the form
+                    thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
+                    // Show the error with the returned error text.
+                    thisForm.find('.form-error').text(errorHTTP).fadeIn(1000);
+                    thisForm.find('.form-success').fadeOut(1000);
+                    $(thisForm).find('.form-loading').remove();
+                    $(thisForm).find('input[type="submit"]').show();
+
+                    console.log("error response: /" + JSON.parse(JSON.stringify(errorObject)) + "/" + errorText + "/" + errorHTTP + "/");
+                }
+            });
+        }
+
         return false;
     });
 
