5c44bd72f9b492645372d6d653e7a153e7fb9549
Adam Tait <adam@adamtait.com>
2016-04-10 08:24:17 -0700

    dependency changes
    + update to Clojure 1.8
    + add tools.logging
    + add google.api.client (not sure if this is still necessary)
    + add appengine-api.sdk (turns out this is very important when running
    on AppEngine)
    + move to java & clojure specific source paths
    + disable run-dev profile because it seemed to be running everytime
    `lein` was called

--- project.clj
+++ project.clj
@@ -5,27 +5,36 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [com.stuartsierra/component "0.2.1"]
                  [compojure "1.1.8"]
                  [javax.servlet/servlet-api "2.5"]
 
                  [hiccup "1.0.5"]
 
+                 [com.google.api-client/google-api-client "1.19.1"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.9.34"]
+                 
                  [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.12"]
                  [org.slf4j/jcl-over-slf4j "1.7.12"]
                  [org.slf4j/log4j-over-slf4j "1.7.12"]]
 
 
-  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :javac-options     ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+
   :min-lein-version "2.0.0"
+
   :resource-paths ["config", "resources"]
+
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [http-kit "2.1.16"]]
-                   :aliases {"run-dev" ["trampoline" "run" "-m" "user/run-dev"]}}
+                   ;;:aliases {"run-dev" ["trampoline" "run" "-m" "user/go"]}
+                   }
              
              :production
              {:ring
@@ -38,7 +47,7 @@
 
   :plugins [[lein-ring "0.9.7"]]
   :ring {
-         :handler com.tamandadam.merrymaking-web.server/ring-app
-         :init com.tamandadam.merrymaking-web.server/ring-init
-         :destroy com.tamandadam.merrymaking-web.server/ring-destroy})
+         :handler com.tamandadam.merrymaking-web.server/app-engine-handlers
+         :init com.tamandadam.merrymaking-web.server/app-engine-start
+         :destroy com.tamandadam.merrymaking-web.server/app-engine-stop})
 
