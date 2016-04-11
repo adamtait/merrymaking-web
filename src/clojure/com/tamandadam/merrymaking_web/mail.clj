eb70cbaaacd14ba24d4121bb51b31f8177fe109c
Adam Tait <adam@adamtait.com>
2016-04-10 19:54:42 -0700

    update mail ns to use new logging
    + bonus: add Tam to the email dest list

--- src/clojure/com/tamandadam/merrymaking_web/mail.clj
+++ src/clojure/com/tamandadam/merrymaking_web/mail.clj
@@ -1,5 +1,5 @@
 (ns com.tamandadam.merrymaking-web.mail
-  (:require [clojure.tools.logging :as log])
+  (:require [com.tamandadam.merrymaking-web.log :refer [log log-error]])
   (:import
    (java.util Properties)
    (javax.mail Message MessagingException Session Transport)
@@ -12,9 +12,9 @@
 (defn source-email-address []
   (email-address
    "noreply@merrymaking-web.appspotmail.com"
-   "RSVP form"))
+   "adamandtam.com RSVP form"))
 
-(defn send [^String dest-address subject message]
+(defn send-email [^String dest-address subject message]
   (let [props (Properties.)
         session (Session/getDefaultInstance props)]
     (try
@@ -27,17 +27,19 @@
                   (.setText message))]
         (Transport/send msg))
       (catch AddressException e
-        (log/error e "Unable to send mail"))
+        (log-error e "Unable to send mail"))
       (catch MessagingException e
-        (log/error e "Unable to send mail")))))
+        (log-error e "Unable to send mail")))))
 
 
 
-(def dest-address
-  (email-address "adam@adamtait.com" "Adam"))
+(def dest-addresses
+  ["adam@adamtait.com"
+   "tamthaopham@gmail.com"])
 
 (defn send-new-rsvp [from-name message]
-  (send
-   dest-address
-   (str "New RSVP from " from-name)
-   message))
+  (doseq [addr dest-addresses]
+    (send-email
+     addr
+     (str "New RSVP from " from-name)
+     message)))
